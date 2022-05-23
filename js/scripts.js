645b0026903fec5e383bb22ea904a47660eb6793
michellevilla <mvh.elle@gmail.com>
2022-05-23 09:07:28 +0200

    Add Jquery js file and code in htmls

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,16 @@
 
-console.log("Hola welcome to my site ;)");
+console.log(text1, text2, text3);
+
+var text1="I love";
+var text2="when you count";
+var text3="me out!"
+
+
+// create an object with your name, age, and hobbies.
+//Assign the object to a variable and tell it to print to your console.
+
+var me= {
+  name: "Michelle",
+  age: 29,
+  hobbies: "painting, walking, writing"
+};
