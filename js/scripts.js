9ad915de7ab1efdb646283d2841c8f219203e8ae
michellevilla <mvh.elle@gmail.com>
2022-05-23 19:24:40 +0200

    Add responsive hamb menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-// create an object with your name, age, and hobbies.
-//Assign the object to a variable and tell it to print to your console.
 
-var me= {
-  name: "Michelle",
-  age: 29,
-  hobbies: "painting, walking, writing"
-};
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
