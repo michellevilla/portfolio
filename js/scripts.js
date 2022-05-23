da272ba141c5fcdc33f16b8e8d22e488a0515977
michellevilla <mvh.elle@gmail.com>
2022-05-23 15:11:45 +0200

    Adds fontawesome code to htmls

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-
-console.log(text1, text2, text3);
-
-var text1="I love";
-var text2="when you count";
-var text3="me out!"
-
-
 // create an object with your name, age, and hobbies.
 //Assign the object to a variable and tell it to print to your console.
 
